471caac8206e705ed4cae31ff19612dec5f7e4de
45gfg9 <mc45@qq.com>
2018-09-26 14:05:35 +0800

    gf

--- script/fx.js
+++ script/fx.js
@@ -1,14 +1,6 @@
 // JavaScript Document
-$('#box').bind('keypress',function(e){
-	if(e.keyCode == "13") {
-		logbox();
-	}
-});
-function logbox() {
-	console.log($('#box').val());
-}
 
-function convert() {
+function convertto() {
 	var in_s=$("#in").val();
 	if (in_s == '') { 
     	return; 
@@ -16,4 +8,8 @@ function convert() {
 	$("#out").val('');
 	for (var i=0; i<$("#in").val().length; i++)
 		$("#out").val($("#out").val() + '&#' + in_s.charCodeAt(i) + ';');
+}
+
+function convertfrom() {
+	$('#in').html($('#out').val());
 }
