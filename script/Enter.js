65e7eb7cb1d53199c01060c61cd7e765e446a329
45gfg9 <mc45@qq.com>
2018-10-21 13:55:27 +0800

    p

--- script/Enter.js
+++ script/Enter.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+
+$('#e1').on('keypress',function(e){
+	if(e.keyCode == "13") {
+		Enter();
+	}
+});
+
+function Enter(){
+	md5=hex_md5($('#e2').val());
+	if(md5=='d41d8cd98f00b204e9800998ecf8427e')return;
+	if(md5=='1a663893e46bacd81483faf0ec893451')location.href='heart';
+	else $('#e2').val('Nope');
+}
