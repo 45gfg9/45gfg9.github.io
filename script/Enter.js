b00261b9422504ca60159942cbb5f941c4d07be3
45gfg9 <mc45@qq.com>
2018-10-21 14:00:56 +0800

    p

--- script/Enter.js
+++ script/Enter.js
@@ -7,8 +7,8 @@ $('#e1').on('keypress',function(e){
 });
 
 function Enter(){
-	md5=hex_md5($('#e2').val());
+	md5=hex_md5($('#e1').val());
 	if(md5=='d41d8cd98f00b204e9800998ecf8427e')return;
 	if(md5=='1a663893e46bacd81483faf0ec893451')location.href='heart';
-	else $('#e2').val('Nope');
+	else $('#e1').val('Nope');
 }
