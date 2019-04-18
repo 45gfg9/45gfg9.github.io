d6199dfc6f739441b2cc09957bdd6ef71936903f
45gfg9 <mc45@qq.com>
2019-04-18 13:31:18 +0800

    fix script issues

--- script/Enter.js
+++ script/Enter.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
 			Enter();
 		}
 	});
-
-	function Enter(){
-		md5=md5($('#e1').val());
-		if(md5=='d41d8cd98f00b204e9800998ecf8427e')return;
-		(md5=='1a663893e46bacd81483faf0ec893451')?location.href='heart':$('#e1').val('Nope');
-	}
 });
+
+function Enter(){
+	md5=md5($('#e1').val());
+	if(md5=='d41d8cd98f00b204e9800998ecf8427e')return;
+	(md5=='1a663893e46bacd81483faf0ec893451')?location.href='heart':$('#e1').val('Nope');
+}
--- script/index.js
+++ script/index.js
@@ -1,13 +1,11 @@
 // JavaScript Document
-$(document).ready(function(){
-	function submittonamediv(){
-		$('#namediv').text('Hello, '+$('#fname').val()+' '+$('#lname').val());
-	}
-	function alertthemwhattheytyped(){
-		console.log('Hohoho');
-		alert($('#input1').val);
-	}
-	function seconddivonclick(){
-		$('#theseconddivinthispage').text('why u click me?');
-	}
-});
+function submittonamediv(){
+	$('#namediv').text('Hello, '+$('#fname').val()+' '+$('#lname').val());
+}
+function alertthemwhattheytyped(){
+	console.log('Hohoho');
+	alert($('#input1').val);
+}
+function seconddivonclick(){
+	$('#theseconddivinthispage').text('why u click me?');
+}
