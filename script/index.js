9a9d3bd1a265b96fda6f4a68dee3e5210aba4244
45gfg9 <mc45@qq.com>
2019-04-21 14:04:52 +0800

    i

--- script/index.js
+++ script/index.js
@@ -2,10 +2,12 @@
 function submittonamediv(){
 	$('#namediv').text('Hello, '+$('#fname').val()+' '+$('#lname').val());
 }
+
 function alertthemwhattheytyped(){
 	console.log('Hohoho');
 	alert($('#input1').val);
 }
+
 function seconddivonclick(){
 	$('#theseconddivinthispage').text('why u click me?');
 }
