23250c5213f66b868f65e8caee48f23735d539fd
45gfg9 <mc45@qq.com>
2019-10-18 23:42:01 +0800

    dpyfjy now is 626-class --- so complicate :(

---
+++ 626-class/Hack.js
@@ -0,0 +1,35 @@
+// The site is moved to www.626-class.com
+
+// --- This is used on study.626-class.com
+$.ajax({
+    url:"/couserStudyHistory/ajax/playertimes",
+    data:{
+        "kpointId":"256",
+        "courseId":"119",
+        "playedTime":"2325"
+    },
+    type:"post",
+    dataType:"text",
+    success:function(result){
+        if(result.success)console.log('Time OK');
+    }
+});
+
+// --- This is used on exam.626-class.com
+$.ajax({
+    type:"POST",
+    dataType:"json",
+    async:false,
+    url:"/front/ajax/addPaperRecord",
+    data:{
+        "objectType": 'termExam',
+        "objectId": $("#kpointId").val(),
+        "questionIds": "830,523,518,504,501,481,480,423,405,404,", // "777,776,768,755,744,395,387,345,336,330,"p5
+        "optionChoices": "B,A,B,B,A,B,B,A,A,A,", // "A,A,C,D,A,B,A,A,B,D,"p5
+        "answerLength": 0,
+        "courseId": $("#kpointId").val()
+    },
+    success:function(result){
+        if(result.success)console.log("Exam OK");
+    }
+});
