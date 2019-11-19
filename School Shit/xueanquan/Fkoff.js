6c4d4523f0a91f209c6bca93ca40560d2d59d5da
45gfg9 <mc45@qq.com>
2019-11-19 16:53:16 +0800

    Add 2 exercises

--- School Shit/xueanquan/Fkoff.js
+++ School Shit/xueanquan/Fkoff.js
@@ -1,31 +1,58 @@
-function ajax_request(url, data)
-{
-    var request = ajax_create_request(context);
-
-    request.obj.open('POST', url, async);
-    request.obj.send(data);
-    
-    return ajax_response(request);
-}
-
-function ajax_create_request(context) {
+function ajax_create_request() {
     var req = Object();
     req.obj = new XMLHttpRequest();
-    req.context = context;
-
+    
     return req;
 }
 
+function ajax_request(url, data) {
+    var request = ajax_create_request();
+
+    request.obj.open('POST', url);
+    request.obj.send(data);
+
+    return new ajax_response(request);
+}
+
 function ajax_response(request) {
+    this.request = request.obj;
+    this.error = null;
     this.value = null;
+    this.context = request.context;
     
     if(request.obj.status == 200) {
-        this.value = object_from_json(request);
-        console.log('OK');
-    else {
-        console.error('HTTP request failed with status: ' + request.obj.status);
+        try {
+            this.value = object_from_json(request);
+            
+            if(this.value && this.value.error) {
+                this.error = this.value.error;
+                this.value = null;
+            }
+        }
+        catch(e) {
+            this.error = new ajax_error(e.name, e.description, e.number);
+        }
     }
-    return this.value;
+    else {
+        this.error = new ajax_error('HTTP request failed with status: ' + request.obj.status, request.obj.status);
+    }
+    
+    return this;
+}
+
+function ajax_error(name, description, number) {
+    this.name = name;
+    this.description = description;
+    this.number = number;
+
+    return this;
+}
+
+function object_from_json(request) {
+    if(Boolean(request.obj.responseXML) || Boolean(request.obj.responseXML.xml))
+        return request.obj.responseXML;
+
+    return request.obj.responseText;
 }
 
 ajax_request(
@@ -63,3 +90,58 @@ ajax_request(
     + '\r\nwatchTime='
     + '\r\nCourseID=842'
 );
+
+ajax_request(
+    '/jiating/ajax/FamilyEduCenter.EscapeSkill.SeeVideo,FamilyEduCenter.ashx?_method=TemplateIn2&_session=rw',
+      'workid=225694'
+    + '\r\nfid=250'
+    + '\r\ntitle=走过花季.雨季'
+    + '\r\nrequire='
+    + '\r\npurpose='
+    + '\r\ncontents='
+    + '\r\ntestwanser=0|0|0'
+    + '\r\ntestinfo=已掌握技能'
+    + '\r\ntestMark=100'
+    + '\r\ntestReulst=1'
+    + '\r\nSiteName='
+    + '\r\nsiteAddrees='
+    + '\r\nwatchTime='
+    + '\r\nCourseID=849'
+);
+
+ajax_request(
+    '/jiating/ajax/FamilyEduCenter.EscapeSkill.SeeVideo,FamilyEduCenter.ashx?_method=TemplateIn2&_session=rw',
+      'workid=226389'
+    + '\r\nfid=254'
+    + '\r\ntitle=实验课安全须知'
+    + '\r\nrequire='
+    + '\r\npurpose='
+    + '\r\ncontents='
+    + '\r\ntestwanser=0|0|0'
+    + '\r\ntestinfo=已掌握技能'
+    + '\r\ntestMark=100'
+    + '\r\ntestReulst=1'
+    + '\r\nSiteName='
+    + '\r\nsiteAddrees='
+    + '\r\nwatchTime='
+    + '\r\nCourseID=850'
+);
+
+$.ajax({
+    type: 'POST',
+    url: '//huodongapi.xueanquan.com/p/ningxia/Topic/topic/platformapi/api/v1/records/sign',
+    dataType: 'json',
+    data: {
+        "specialId": 369,
+        "step": 1
+    },
+    xhrFields: {
+        withCredentials: true
+    },
+    success: function(resp) {
+        if (!resp.result) {
+            ShowMsg(resp.msg);
+        }
+    }
+});
+
